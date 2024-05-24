6fcd6aeb61da487752b9da1081acbca58d721d19
JanooGwan <janoogwan39@gmail.com>
2024-05-24 20:51:42 +0900

    ArticleController 작성

--- springjdbc/src/main/java/StudyJDBC/springjdbc/controller/ArticleController.java
+++ springjdbc/src/main/java/StudyJDBC/springjdbc/controller/ArticleController.java
@@ -1,4 +1,57 @@
 package StudyJDBC.springjdbc.controller;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import StudyJDBC.springjdbc.controller.dto.*;
+import StudyJDBC.springjdbc.service.ArticleService;
+
+@RestController
+@RequestMapping("/articles")
 public class ArticleController {
+    private final ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ArticleResponse>> getArticles() {
+        List<ArticleResponse> response = articleService.getAll();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ArticleResponse> getArticle(@PathVariable Long id) {
+        ArticleResponse response = articleService.getById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createArticle(
+            @RequestBody ArticleCreateRequest request
+    ) {
+        ArticleResponse response = articleService.create(request);
+        return ResponseEntity.created(URI.create("/articles/" +response.id())).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ArticleResponse> updateArticle(
+            @PathVariable Long id,
+            @RequestBody ArticleUpdateRequest request
+    ) {
+        ArticleResponse response = articleService.update(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> updateArticle(
+            @PathVariable Long id
+    ) {
+        articleService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
---
+++ springjdbc/src/main/java/StudyJDBC/springjdbc/service/ArticleService.java
@@ -0,0 +1,70 @@
+package StudyJDBC.springjdbc.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import StudyJDBC.springjdbc.controller.dto.*;
+import StudyJDBC.springjdbc.domain.*;
+import StudyJDBC.springjdbc.repository.*;
+
+@Service
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+    private final MemberRepository memberRepository;
+    private final BoardRepository boardRepository;
+
+    public ArticleService(
+            ArticleRepository articleRepository,
+            MemberRepository memberRepository,
+            BoardRepository boardRepository
+    ) {
+        this.articleRepository = articleRepository;
+        this.memberRepository = memberRepository;
+        this.boardRepository = boardRepository;
+    }
+
+    public List<ArticleResponse> getAll() {
+        List<Article> articles = articleRepository.findAll();
+        return articles.stream()
+                .map(article -> {
+                    Member member = memberRepository.findById(article.getWriterId());
+                    Board board = boardRepository.findById(article.getBoardId());
+                    return ArticleResponse.of(article, member, board);
+                }).toList();
+    }
+
+    public ArticleResponse getById(Long id) {
+        Article article = articleRepository.findById(id);
+        Member member = memberRepository.findById(article.getWriterId());
+        Board board = boardRepository.findById(article.getBoardId());
+        return ArticleResponse.of(article, member, board);
+    }
+    public ArticleResponse create(ArticleCreateRequest request) {
+        Article article = new Article(
+                request.writerId(),
+                request.boardId(),
+                request.title(),
+                request.description(),
+                LocalDateTime.now()
+        );
+        Article saved = articleRepository.insert(article);
+        Member member = memberRepository.findById(saved.getWriterId());
+        Board board = boardRepository.findById(article.getBoardId());
+        return ArticleResponse.of(article, member, board);
+    }
+
+    public ArticleResponse update(Long id, ArticleUpdateRequest request) {
+        Article article = articleRepository.findById(id);
+        article.update(request.boardId(), request.title(), request.description());
+        Article updated = articleRepository.update(id, article);
+        Member member = memberRepository.findById(updated.getWriterId());
+        Board board = boardRepository.findById(article.getBoardId());
+        return ArticleResponse.of(article, member, board);
+    }
+
+    public void delete(Long id) {
+        articleRepository.deleteById(id);
+    }
+}
