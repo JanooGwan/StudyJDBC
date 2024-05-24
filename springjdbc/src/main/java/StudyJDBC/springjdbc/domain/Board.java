53101a913c9f4df593b878dbe9554f5a0e8180ba
JanooGwan <janoogwan39@gmail.com>
2024-05-24 18:10:43 +0900

    Board : Domain 작성

--- springjdbc/src/main/java/StudyJDBC/springjdbc/domain/Board.java
+++ springjdbc/src/main/java/StudyJDBC/springjdbc/domain/Board.java
@@ -1,4 +1,22 @@
 package StudyJDBC.springjdbc.domain;
 
 public class Board {
+    private int id;
+    private String name;
+
+    public Board(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
