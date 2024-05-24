27db68214068966b729d82bdbfff3b16ba30c5b3
JanooGwan <janoogwan39@gmail.com>
2024-05-24 18:13:42 +0900

    Member : Domain 작성

--- springjdbc/src/main/java/StudyJDBC/springjdbc/domain/Member.java
+++ springjdbc/src/main/java/StudyJDBC/springjdbc/domain/Member.java
@@ -1,4 +1,34 @@
 package StudyJDBC.springjdbc.domain;
 
 public class Member {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    public Member(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
